5890bcdf2f6832837f259888dc98fcac2511a2b1
godotgodo <yunusemregolbasi0@gmail.com>
2024-07-26 17:15:38 +0300

    feat(school) add school crud operations

--- src/controllers/SchoolController.ts
+++ src/controllers/SchoolController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import SchoolService from '../services/SchoolService';
+import mongoose from 'mongoose';
 
 class SchoolController {
   private schoolService: SchoolService;
@@ -17,6 +18,76 @@ class SchoolController {
       const userId = req.user._id;
       const school = await this.schoolService.createSchool(userId, req.body);
       res.status(201).sendCustomBody(school);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async updateSchool(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const userId = req.user._id;
+      const schoolId = new mongoose.Types.ObjectId(req.params.id);
+
+      const updatedSchool = await this.schoolService.updateSchool(
+        userId,
+        schoolId,
+        req.body,
+      );
+
+      res.status(200).sendCustomBody(updatedSchool);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async deleteSchool(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const userId = req.user.id;
+      const schoolId = new mongoose.Types.ObjectId(req.params.id);
+
+      const deletedSchool = await this.schoolService.deleteSchool(
+        userId,
+        schoolId,
+      );
+
+      res.status(200).sendCustomBody(deletedSchool);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async getAll(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const userId = req.user._id;
+      const schools = await this.schoolService.getAll(userId);
+      res.status(200).send(schools);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async getById(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const userId = req.user._id;
+      const schoolId = new mongoose.Types.ObjectId(req.params.id);
+      const school = await this.schoolService.getSchoolById(userId, schoolId);
+      res.status(200).sendCustomBody(school);
     } catch (error) {
       next(error);
     }
--- src/services/SchoolService.ts
+++ src/services/SchoolService.ts
@@ -1,7 +1,45 @@
-import { ObjectId } from 'mongoose';
+import mongoose, { ObjectId } from 'mongoose';
 import User, { ISchool } from '../models/User';
 
 class SchoolService {
+  public async getAll(userId: mongoose.Types.ObjectId): Promise<ISchool[]> {
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw { message: 'User not found', statusCode: 404 };
+      }
+
+      return user.schools;
+    } catch (error) {
+      throw {
+        message: 'Failed to retrieve schools',
+        statusCode: 500,
+      };
+    }
+  }
+
+  public async getSchoolById(
+    userId: mongoose.Types.ObjectId,
+    schoolId: mongoose.Types.ObjectId,
+  ): Promise<ISchool> {
+    try {
+      const user = await User.findOne({
+        _id: userId,
+        'schools._id': schoolId,
+      }).exec();
+      if (!user) {
+        throw { message: 'User or school not found', statusCode: 404 };
+      }
+
+      return user.schools[user.schools.length - 1];
+    } catch (error) {
+      throw {
+        message: 'Failed to retrieve school',
+        statusCode: 500,
+      };
+    }
+  }
+
   public async createSchool(
     userId: ObjectId,
     schoolData: ISchool,
@@ -23,6 +61,53 @@ class SchoolService {
       }
     } catch (error) {
       throw { message: 'Failed to create school', statusCode: 500 };
+    }
+  }
+
+  public async updateSchool(
+    userId: mongoose.Types.ObjectId,
+    schoolId: mongoose.Types.ObjectId,
+    schoolData: Partial<ISchool>,
+  ): Promise<ISchool> {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId, 'schools._id': schoolId },
+        { $set: { 'schools.$': schoolData } },
+        { new: true },
+      );
+
+      if (!updatedUser) {
+        throw { message: 'School or user not found', statusCode: 404 };
+      }
+
+      //temporarily
+      return updatedUser.schools[updatedUser.schools.length - 1];
+    } catch (error) {
+      throw {
+        message: 'Failed to update school',
+        statusCode: 500,
+      };
+    }
+  }
+
+  public async deleteSchool(
+    userId: mongoose.Types.ObjectId,
+    schoolId: mongoose.Types.ObjectId,
+  ): Promise<ISchool> {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: userId, 'schools._id': schoolId },
+        { $pull: { schools: { _id: schoolId } } },
+        { new: true },
+      );
+
+      if (!user) {
+        throw { message: 'User or school not found', statusCode: 404 };
+      }
+      //temporarly
+      return user.schools[user.schools.length - 1];
+    } catch (error) {
+      throw { message: 'School delate failed', statusCode: 500 };
     }
   }
 }
